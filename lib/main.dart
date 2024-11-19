0d4359701b4a98a9d3a2bf2fe2148a1c7b460f92
ChristinaBustos <105693387+ChristinaBustos@users.noreply.github.com>
2024-11-18 18:24:02 -0600

    [FIX] erro sending

--- lib/main.dart
+++ lib/main.dart
@@ -5,22 +5,20 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 Future<void> _firebaseMessagingHandler(RemoteMessage message) async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  print("Estoy capturando el mensaje: ${message.notification!.title}");
+  print("Estoy capturando el mensaje: ${message.notification?.title}");
 }
 
-const AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings('@mipmap/ic_lancher');
-final DarwinInitializationSettings initializationSettingsDarwin = const DarwinInitializationSettings();
+const AndroidInitializationSettings initializationSettingsAndroid =
+AndroidInitializationSettings('@mipmap/ic_launcher');
+final DarwinInitializationSettings initializationSettingsDarwin =
+DarwinInitializationSettings();
 final InitializationSettings initializationSettings = InitializationSettings(
   android: initializationSettingsAndroid,
-  iOS: initializationSettingsDarwin
+  iOS: initializationSettingsDarwin,
 );
-// adasdasds
 
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+FlutterLocalNotificationsPlugin();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -34,60 +32,53 @@ void main() async {
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
-   @override
-   State<MyApp> createState() => _MyAppState();
+  @override
+  State<MyApp> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp>{
+class _MyAppState extends State<MyApp> {
   @override
-  void initState(){
+  void initState() {
     super.initState();
     FirebaseMessaging messaging = FirebaseMessaging.instance;
     messaging.requestPermission(
       alert: true,
-      announcement: false,
-      badge: false,
-      carPlay: false,
-      criticalAlert: false,
-      provisional: false,
-      sound: true
+      badge: true,
+      sound: true,
     );
 
-    FirebaseMessaging.onMessage.listen((RemoteMessage remoteMessage){
-      print("Estoy obteniendo una notidicación");
-      print("${remoteMessage.data}");
+    FirebaseMessaging.onMessage.listen((RemoteMessage remoteMessage) {
+      print("Estoy obteniendo una notificación: ${remoteMessage.notification?.title}");
       flutterLocalNotificationsPlugin.show(
         remoteMessage.hashCode,
         remoteMessage.notification?.title,
         remoteMessage.notification?.body,
         const NotificationDetails(
           android: AndroidNotificationDetails(
-            "Id de Prueba",
-             "Canal de prueba",
-             channelDescription: 'Desmostartion',
-             importance: Importance.max,
-             priority: Priority.max )
-        )
+            "test_channel",
+            "Test Notifications",
+            channelDescription: 'Canal para pruebas',
+            importance: Importance.max,
+            priority: Priority.high,
+          ),
+        ),
       );
-      if(remoteMessage.notification!=null){
-        print("object':${remoteMessage.notification}");
-      }
-      
     });
-    FirebaseMessaging.instance.getToken().then((token){
+
+    FirebaseMessaging.instance.getToken().then((token) {
       print("Token del dispositivo: $token");
     });
   }
 
   @override
   Widget build(BuildContext context) {
-   return  MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(title: const Text("Notificaciones Push"),),
-      body: const Center(
-        child: Text("Esperando Notifiaciones"),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Notificaciones Push")),
+        body: const Center(
+          child: Text("Esperando Notificaciones"),
+        ),
       ),
-    ),
-   );
+    );
   }
 }
